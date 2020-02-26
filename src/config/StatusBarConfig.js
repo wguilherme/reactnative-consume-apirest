40f1a0525e1fead534569d1b92e314d517c2da48
W. Guilherme <ravok123@gmail.com>
2020-02-25 22:06:28 -0300

    [Update] HeaderBar Style + Header config for iOS / Android

---
+++ src/config/StatusBarConfig.js
@@ -0,0 +1,4 @@
+import {StatusBar} from 'react-native';
+
+StatusBar.setBackgroundColor('#DA552F'); // android bg
+StatusBar.setBarStyle('light-content'); // ios text-white
--- src/pages/main.js
+++ src/pages/main.js
@@ -3,6 +3,9 @@ import React, {Component} from 'react';
 import {Text, View} from 'react-native';
 
 export default class Main extends Component {
+  static navigationOptions = {
+    title: 'Eae',
+  };
   render() {
     return (
       <View>
