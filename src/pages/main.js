abb2bc5ec0e3f71948225f6665033f834fbd7621
W. Guilherme <ravok123@gmail.com>
2020-02-25 23:53:29 -0300

    [UPDATE] Consume API Rest with Axios and display into main page

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,15 +1,38 @@
 import React, {Component} from 'react';
-
 import {Text, View} from 'react-native';
+import api from '../services/api';
 
 export default class Main extends Component {
   static navigationOptions = {
-    title: 'Eae',
+    title: 'Main Page',
   };
+  //state
+
+  state = {
+    pets: [],
+  };
+
+  // //call api
+  componentDidMount() {
+    this.loadProducts();
+  }
+
+  loadProducts = async () => {
+    const response = await api.get('/pets');
+
+    this.setState({pets: response.data});
+    console.log(response.data);
+  };
+
   render() {
+    const {pets} = this.state;
     return (
       <View>
-        <Text> textInComponent </Text>
+        <Text> aeae </Text>
+
+        {this.state.pets.map(item => (
+          <Text key={item._id}>{item.nome}</Text>
+        ))}
       </View>
     );
   }
